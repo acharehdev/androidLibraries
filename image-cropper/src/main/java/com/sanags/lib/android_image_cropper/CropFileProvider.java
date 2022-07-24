01cf0adfa7e7fc9a3f9802498593fe74995f5c35
Mahdi <nmahdi1466@gmail.com>
2022-07-24 16:29:54 +0430

    init

--- image-cropper/src/main/java/com/sanags/lib/android_image_cropper/CropFileProvider.java
+++
@@ -1,6 +0,0 @@
-package com.sanags.lib.android_image_cropper;
-
-import androidx.core.content.FileProvider;
-
-public class CropFileProvider extends FileProvider {
-}
---
+++ image-cropper/src/main/java/ir/sanags/android/image_cropper/CropFileProvider.java
@@ -0,0 +1,6 @@
+package ir.sanags.android.image_cropper;
+
+import androidx.core.content.FileProvider;
+
+public class CropFileProvider extends FileProvider {
+}
